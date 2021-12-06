054ad30d41142b203b3454e1247bc8ab6fa6916f
HannahWhelan256 <hannahwhelan256@gmail.com>
2021-12-06 16:21:58 +0000

    task 2c

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ let myCollection = [
   ];
 
   console.log(myCollection);
+
+  function describeItem(item) {
+    if (item.count > 1) {
+      console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+      } else {
+      console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
+      }
+  };
+
+  describeItem(myCollection[1]);
